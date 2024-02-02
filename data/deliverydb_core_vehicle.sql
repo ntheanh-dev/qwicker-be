64a662da10ff967892067224e62109e4331123a4
Theanhmgt <theanhmgt66@gmail.com>
2024-02-02 11:31:30 +0700

    feat(data): update backup sql data

--- data/deliverydb_core_vehicle.sql
+++ data/deliverydb_core_vehicle.sql
@@ -21,7 +21,11 @@
 
 LOCK TABLES `core_vehicle` WRITE;
 /*!40000 ALTER TABLE `core_vehicle` DISABLE KEYS */;
-INSERT INTO `core_vehicle` VALUES (2,'Xe Máy','Vận chuyển mặt hàng nhỏ giá trị đến 3 triệu đồng','image/upload/v1706106196/vehicle_icon/gjisuqtnu1gl7rtpdron.png','0.5 x 0.4 x 0.5 Mét - Lên đến 30kg'),(3,'Xe Van 500 kg','Hoạt Động Tất Cả Khung Giờ | Chở Tối Đa 500Kg * 1.5CBM','image/upload/v1706106556/vehicle_icon/pkbqdybiilwiynh0yyxv.png','1.7 x 1.2 x 1.2 Mét Lên đến 500 kg'),(4,'Xe Van 1000 kg','Hoạt Động Tất Cả Khung Giờ | Chở Tối Đa 1000Kg * 4CBM','image/upload/v1706106626/vehicle_icon/rqqk1cvmbt7q5agsgdry.png','1.7 x 1.2 x 1.2 Mét Lên đến 500 kg'),(5,'Xe Tải 500kg','Giờ Cấm Tải 6H-9H & 16H-20H | Chở tối đa 500Kg & 1.5CBM','image/upload/v1706106669/vehicle_icon/lnnx9evnqsxy1gmygn23.png','1.9 x 1.4 x 1.4 Mét Lên đến 500 kg'),(6,'Xe Tải 1000kg','Giờ Cấm Tải 6H-9H & 16H-20H | Chở tối đa 1000Kg & 5CBM','image/upload/v1706106704/vehicle_icon/enknv9eqjzcdpc10jxxo.png','3 x 1.6 x 1.6 Mét Lên đến 1000 kg');
+INSERT INTO `core_vehicle` VALUES (2,'Xe Máy','Vận chuyển mặt hàng nhỏ giá trị đến 3 triệu đồng','0.5 x 0.4 x 0.5 Mét - Lên đến 30kg','image/upload/v1706106196/vehicle_icon/gjisuqtnu1gl7rtpdron.png')
+,(3,'Xe Van 500 kg','Hoạt Động Tất Cả Khung Giờ | Chở Tối Đa 500Kg * 1.5CBM','1.7 x 1.2 x 1.2 Mét Lên đến 500 kg','image/upload/v1706106556/vehicle_icon/pkbqdybiilwiynh0yyxv.png')
+,(4,'Xe Van 1000 kg','Hoạt Động Tất Cả Khung Giờ | Chở Tối Đa 1000Kg * 4CBM','1.7 x 1.2 x 1.2 Mét Lên đến 500 kg','image/upload/v1706106626/vehicle_icon/rqqk1cvmbt7q5agsgdry.png')
+,(5,'Xe Tải 500kg','Giờ Cấm Tải 6H-9H & 16H-20H | Chở tối đa 500Kg & 1.5CBM','1.9 x 1.4 x 1.4 Mét Lên đến 500 kg','image/upload/v1706106669/vehicle_icon/lnnx9evnqsxy1gmygn23.png')
+,(6,'Xe Tải 1000kg','Giờ Cấm Tải 6H-9H & 16H-20H | Chở tối đa 1000Kg & 5CBM','3 x 1.6 x 1.6 Mét Lên đến 1000 kg','image/upload/v1706106704/vehicle_icon/enknv9eqjzcdpc10jxxo.png');
 /*!40000 ALTER TABLE `core_vehicle` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
